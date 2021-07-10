578ccee213231e12fad80a003917e956db528bcb
Michael Carmody <mr.carmody89@gmail.com>
2021-07-09 23:59:57 -0400

    Updated for Now

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,37 @@
 // Assignment Code
 
-//function regenerate() {
-// Write password to the #password input
-
-//if (!guestuser) {
-//  return;
-//}
-
+//Alerts and prompts were easily established. 
 var guestuser = alert ("Good day! Please create a password!");
 var guestuser = alert ("The Password will be between 8 and 128 characters long.");
 var guestuser = alert ("You must choose between lowercase, uppercase, numeric and/or special characters.");
 
-
+//The struggle with the prompts and password variables stemmed from the inability
+//to create the correct parse, if else, and random functions to tie everything together.
+var lcletters = "abcdefghijklmnopqrstuvwxyz";
+var ucletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var allnumbers = "0123456789";
+var spec_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 var generateBtn = document.querySelector("#generate"); 
+var generatePassword = lcletters + ucletters + allnumbers + spec_char
 
-//var randomchars = Math.floor(Math.random() * chars.length);
-
-
+//established the relationship of the 'var password = generatePassword' too late. 
+//I will continue to work with generate password and build the functionality out 
+//of that so that it translate into the writePassword function. 
 function writePassword() {
-  var lcletters = "abcdefghijklmnopqrstuvwxyz";
-  var ucletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var allnumbers = "0123456789";
-  var spec_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   var pwlength = prompt ("Let's begin! Please enter the number of characters you would like in the password");
-  
   var lowercase = prompt ("Would you like lowercase? Please type yes or no.");
-  if (8 < string_length > 128) parseFloat(InputEvent);
-  var Uppercase = prompt ("would you like uppercase? Please type yes or no.");
-  if (8 < string_length > 128) parseFloat(InputEvent);
+  var Uppercase =  prompt ("Would you like Uppercase? Please type yes or no.");
   var number = prompt ("Would you like a number? Please type yes or no.");
-  if (8 < string_length > 128) parseFloat(InputEvent);
   var special_char = prompt ("Would you like a special character? Please type yes or no.");
-  if (8 < string_length > 128) parseFloat(InputEvent);
-   //var string_length = 8 - 128;
-  //var randomstring = '';
-  var password = generatePassword();
+  var password = generatePassword;
   var passwordText = document.querySelector("#password");
-
+    
   passwordText.value = password;
-  
+
 };
-  //for (var i=0; i < chars.length; i++) {
-    //  var chars = Math.floor(Math.random() * chars.length);
-      //randomstring += chars.substring(chars,chars+1);
-  //}
-  //document.randform.randomfield.value = randomstring;
-  
 
-
+    
+    
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//}
